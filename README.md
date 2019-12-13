4e5467abdced349e2a9d041e3868553d6bc51aa6
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-12-13 16:30:22 +0900

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # aws_backup_script
+
+need this repo, too.  
+https://github.com/IRISMeister/azure_backup_script.git
---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+./preScript.sh
+
+#Do I need this?
+#for target in $(findmnt -nlo TARGET -t ext4); do fsfreeze -f $target; done
+
+instance=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+region=`curl -s 169.254.169.254/latest/meta-data/placement/availability-zone`
+region=${region::-1}
+
+# add --filter whatever you'd like
+volumes=`aws ec2 describe-instance-attribute --instance-id $instance --attribute blockDeviceMapping --output text --query BlockDeviceMappings[*].Ebs.VolumeId --region $region`
+
+# adding volume-id, tag(Name) to description would be nice...
+for volume in $(echo $volumes | tr " " "\n")
+  do aws ec2 create-snapshot --volume-id $volume --description 'Consistent snapshot of IRIS' --region $region > /dev/null 2>&1
+done
+
+./postScript.sh
