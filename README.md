9bd7958ee991ba7e6523633a745382850b8f1ed5
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-12-17 11:40:01 +0900

    improved

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Inspired by  
 https://aws.amazon.com/jp/blogs/compute/automating-the-creation-of-consistent-amazon-ebs-snapshots-with-amazon-ec2-systems-manager-part-1/
 
-
 need shell scripts from this repo, too.  
 https://github.com/IRISMeister/azure_backup_script.git
+
--- backup.sh
+++ backup.sh
@@ -2,23 +2,21 @@
 # freezing IRIS DBs
 ./preScript.sh
 
-#Do I want to do this?
-#for target in $(findmnt -nlo TARGET -t ext4); do fsfreeze -f $target; done
-
 instance=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 region=`curl -s 169.254.169.254/latest/meta-data/placement/availability-zone`
 region=${region::-1}
 
-# add --filter whatever you'd like
+# add --filter whatever you'd like, otherwise you will get all volumes this instance is attaching.
+# ex) --filters "Name=tag-key,Values=Backup" "Name=tag-value,Values=1"
 volumes=`aws ec2 describe-instance-attribute --instance-id $instance --attribute blockDeviceMapping --output text --query BlockDeviceMappings[*].Ebs.VolumeId --region $region`
 
-# adding volume-id, tag(Name) to description would be nice...
+# adding volume-id, tag(Name) to description would be nice for later use (restore).
 for volume in $(echo $volumes | tr " " "\n")
-  do aws ec2 create-snapshot --volume-id $volume --description 'Consistent snapshot of IRIS' --region $region > /dev/null 2>&1
+  # expecting only one device here
+  device=`aws ec2 describe-volumes --volume-ids $volume --output text --query Volumes[0].Attachments[*].[Device] --region $region`
+  #do aws ec2 create-snapshot --volume-id $volume --description 'Consistent snapshot of IRIS ' --tag-specifications 'Tags=$tags' --region $region > /dev/null 2>&1
+  do aws ec2 create-snapshot --volume-id $volume --description 'Consistent snapshot of IRIS on '$device --tag-specifications 'ResourceType=snapshot,Tags=[{Key=device,Value='$device'}]' --region $region > /dev/null 2>&1
 done
-
-#Do I want to do this?
-#for target in $(findmnt -nlo TARGET -t ext4); do fsfreeze -u $target; done
 
 # Thawing IRIS DBs
 ./postScript.sh
