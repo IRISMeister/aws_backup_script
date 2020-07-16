ed29e69ed2960dc390882748c3011cc702abbc85
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-07-16 17:23:18 +0900

    --filters doesn't work anymore...

--- backup.sh
+++ backup.sh
@@ -6,16 +6,28 @@ instance=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 region=`curl -s 169.254.169.254/latest/meta-data/placement/availability-zone`
 region=${region::-1}
 
-# add --filter whatever you'd like, otherwise you will get all volumes this instance is attaching.
-# ex) --filters "Name=tag-key,Values=Backup" "Name=tag-value,Values=1"
 volumes=`aws ec2 describe-instance-attribute --instance-id $instance --attribute blockDeviceMapping --output text --query BlockDeviceMappings[*].Ebs.VolumeId --region $region`
 
-# adding volume-id, tag(Name) to description would be nice for later use (restore).
 for volume in $(echo $volumes | tr " " "\n")
-  # expecting only one device here
-do 
-  device=`aws ec2 describe-volumes --volume-ids $volume --output text --query Volumes[0].Attachments[*].[Device] --region $region`
-  aws ec2 create-snapshot --volume-id $volume --description 'Consistent snapshot of IRIS on '$device --tag-specifications 'ResourceType=snapshot,Tags=[{Key=device,Value='$device'}]' --region $region > /dev/null 2>&1
+do
+  nametag=`aws ec2 describe-tags --filters "Name=resource-id,Values=$volume" --query Tags[0].Value`
+  echo "nametag:"$nametag
+  # Example to process only volumes which have following TAG. Otherwise all attached devices are processed.
+  #    "Tags": [
+  #      {
+  #          "Key": "Name",
+  #          "ResourceId": "vol-xxxxxxxxxxxxxxxx",
+  #          "ResourceType": "volume",
+  #          "Value": "Test-0001-data"
+  #      }
+  #  ]
+  if [[ $nametag == *-data* ]];
+  then
+    device=`aws ec2 describe-volumes --volume-ids $volume --output text --query Volumes[0].Attachments[*].[Device] --region $region`
+    echo $device
+   # adding TAG, such as device:xvdc, for convinience (usefull when restoring).
+    aws ec2 create-snapshot --volume-id $volume --description 'Consistent snapshot of IRIS on '$device --tag-specifications 'ResourceType=snapshot,Tags=[{Key=device,Value='$device'}]' --region $region > /dev/null 2>&1
+  fi
 done
 
 # Thawing IRIS DBs
